c55d2619ba49475781b06c95d93b6b80026665a5
Amith <84448311+amithplatformatory@users.noreply.github.com>
2021-05-31 15:49:14 +0530

    Add files via upload

---
+++ tls.tf
@@ -0,0 +1,29 @@
+#
+# Self-Signed TLS Certificate for Testing
+#
+
+resource "tls_private_key" "second" {
+  algorithm = "RSA"
+}
+
+resource "tls_self_signed_cert" "second" {
+  allowed_uses = [
+    "key_encipherment",
+    "digital_signature",
+    "server_auth",
+  ]
+  dns_names             = [var.rest_api_domain_name]
+  key_algorithm         = tls_private_key.second.algorithm
+  private_key_pem       = tls_private_key.second.private_key_pem
+  validity_period_hours = 12
+
+  subject {
+    common_name  = var.rest_api_domain_name
+    organization = "ACME Examples, Inc"
+  }
+}
+
+resource "aws_acm_certificate" "second" {
+  certificate_body = tls_self_signed_cert.second.cert_pem
+  private_key      = tls_private_key.second.private_key_pem
+}
