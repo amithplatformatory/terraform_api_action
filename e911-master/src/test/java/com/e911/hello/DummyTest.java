6af4f1551af055c2cfaa28d504cdf663787d7e1a
S Amith Nadig <amith304@gmail.com>
2021-05-31 08:17:23 +0530

    added e911

---
+++ e911-master/src/test/java/com/e911/hello/DummyTest.java
@@ -0,0 +1,13 @@
+package com.e911.hello;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DummyTest {
+
+  @Test
+  public void aTest() {
+    assertEquals(true, true);
+  }
+}
